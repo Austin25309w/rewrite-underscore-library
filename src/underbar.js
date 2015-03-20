467319c5b755b5a4f4f6bcbaebb5e058d55bbb64
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-20 15:21:50 -0500

    update reject function

--- src/underbar.js
+++ src/underbar.js
@@ -100,10 +100,8 @@
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
     return _.filter(collection, function(item) {
-      if (!test(item)) {
-        return item;
-      }
-    })
+      return !test(item);
+    });
   };
 
   // Produce a duplicate-free version of the array.
